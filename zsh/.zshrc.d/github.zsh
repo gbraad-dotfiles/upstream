be5275b8d3e140f6a2b5efb193f27071cdd36054
Gerard Braad <me@gbraad.nl>
2025-02-19 22:53:59 +0800

    Add ghcli secret store

--- zsh/.zshrc.d/github.zsh
+++ zsh/.zshrc.d/github.zsh
@@ -2,7 +2,7 @@
 
 alias login-ghcr='podman login ghcr.io -u USERNAME -p $(secrets get ghcr_pat)'
 
-_install_gh_debian() {
+_install_ghcli_debian() {
   sudo mkdir -p -m 755 /etc/apt/keyrings \
   out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg \
   cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg > /dev/null \
@@ -12,32 +12,38 @@ _install_gh_debian() {
   sudo apt install gh -y
 }
 
-_install_gh_fedora() {
+_install_ghcli_fedora() {
   # DNF5
   sudo dnf install dnf5-plugins
   sudo dnf config-manager addrepo --from-repofile=https://cli.github.com/packages/rpm/gh-cli.repo
   sudo dnf install gh --repo gh-cli
 }
 
-_install_gh_centos() {
+_install_ghcli_centos() {
   # DNF4
   sudo dnf install 'dnf-command(config-manager)'
   sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
   sudo dnf install gh --repo gh-cli
 }
 
-_install_gh() {
+_install_ghcli() {
   source /etc/os-release
   case "$ID" in
     "fedora")
-      _install_gh_fedora
+      _install_ghcli_fedora
       ;;
     "debian" | "ubuntu")
-      _install_gh_debian
+      _install_ghcli_debian
       ;;
     "centos" | "rhel")
-      _install_gh_centos
+      _install_ghcli_centos
       ;;
   esac
 }
-alias install-gh=_install_gh
+alias install-ghcli=_install_ghcli
+
+_secret_ghcli() {
+  mkdir -p ${HOME}/.config/gh
+  secrets file ghcli ${HOME}/.config/gh/hosts.yml
+}
+alias install-ghcli-secret=_secret_ghcli
