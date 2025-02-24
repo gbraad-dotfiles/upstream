703ab5ab40f50e9df0131b6160f1ac5739adce3c
Gerard Braad <me@gbraad.nl>
2025-02-24 11:59:14 +0800

    Add Linuxbreww paths when installed

--- zsh/.zshrc.d/brew.zsh
+++ zsh/.zshrc.d/brew.zsh
@@ -5,3 +5,13 @@ _installhomebrew() {
   bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 }
 alias install-homebrew=_installhomebrew
+
+if [ -d "/home/linuxbrew/.linuxbrew" ]; then
+  export HOMEBREW_PREFIX="/home/linuxbrew/.linuxbrew";
+  export HOMEBREW_CELLAR="/home/linuxbrew/.linuxbrew/Cellar";
+  export HOMEBREW_REPOSITORY="/home/linuxbrew/.linuxbrew/Homebrew";
+  fpath[1,0]="/home/linuxbrew/.linuxbrew/share/zsh/site-functions";
+  export PATH="/home/linuxbrew/.linuxbrew/bin:/home/linuxbrew/.linuxbrew/sbin${PATH+:$PATH}";
+  [ -z "${MANPATH-}" ] || export MANPATH=":${MANPATH#:}";
+  export INFOPATH="/home/linuxbrew/.linuxbrew/share/info:${INFOPATH:-}";
+fi
