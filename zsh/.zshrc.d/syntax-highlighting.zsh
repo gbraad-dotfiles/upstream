1e3d1dead1136ea041a42c382a19526b492fd7c6
Gerard Braad <me@gbraad.nl>
2025-02-27 13:48:47 +0800

    Update syntax-highlighting.zsh

--- zsh/.zshrc.d/syntax-highlighting.zsh
+++ zsh/.zshrc.d/syntax-highlighting.zsh
@@ -2,3 +2,6 @@
 #source $HOME/Projects/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
 #ZSH_HIGHLIGHT_HIGHLIGHTERS=(main brackets pattern cursor)
 #ZSH_HIGHLIGHT_PATTERNS+=('rm -rf' 'fg=white,bold,bg=red')
+
+# Highlight - used by ranger
+export HIGHLIGHT_STYLE=base16/bright 
