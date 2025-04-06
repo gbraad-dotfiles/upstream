9f186490077e03f89e5853ce61fea1097fc21f87
Gerard Braad <me@gbraad.nl>
2025-04-06 18:51:04 +0800

    [skip actions] Add codium server download

--- zsh/.zshrc.d/code.zsh
+++ zsh/.zshrc.d/code.zsh
@@ -9,6 +9,44 @@ alias codeini="git config -f $CONFIG"
 _codepath=$(codeini --get code.path || echo "${HOME}/.local/bin")
 eval _codepath=$(echo ${_codepath})
 
+_installcodiumserverdownload() {
+    local install_location="$1"
+  
+    API_URL="https://api.github.com/repos/VSCodium/vscodium/releases/latest"
+    response=$(curl -s "$API_URL")
+    download_url=$(echo "$response" | grep -oP '(?<="browser_download_url": ")[^"]*vscodium-reh-web-linux-x64[^"]*.tar.gz(?!.*sha)')
+    version=$(echo "$download_url" | grep -oP '(?<=/download/)[^/]+')
+
+    if [[ -z "$download_url" ]]; then
+        echo "Download URL not found"
+        return
+    fi
+
+    temp_file=$(mktemp)
+    curl -L -o "$temp_file" "$download_url"
+  
+    install_dir="$install_location/$version"
+    mkdir -p "$install_dir"
+    tar -xzf "$temp_file" -C "$install_dir"
+    ln -sfn "$install_dir" "$install_location/latest"
+    rm "$temp_file"
+}
+
+_installcodiumserversystem() {
+    install_location=/opt/codium
+    _installcodiumserverdownload $install_location
+    sudo ln -sfn "$install_location/latest/bin/codium-server" "/usr/bin/codium-server"
+}
+
+_installcodiumserveruser() {
+    install_location=${HOME}/.codium
+    _installcodiumserverdownload $install_location
+    ln -sfn "$install_location/latest/bin/codium-server" "${HOME}/.local/bin/codium-server"
+}
+
+alias install-codiumserver-user="_installcodiumserveruser"
+alias install-codiumserver-system="_installcodiumserversystem"
+
 _codeexists() {
     if [ -f "${_codepath}/code" ]; then
         return 0  # File exists, return true (0)
