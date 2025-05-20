af7b8fb14563dcd04c702a0dafb0bfc959df5f49
Gerard Braad <me@gbraad.nl>
2025-05-21 00:18:17 +0800

    [skip actions] autoload dotfiles for ipython

--- ipython/.ipython/profile_default/ipython_config.py
+++ ipython/.ipython/profile_default/ipython_config.py
@@ -2,4 +2,4 @@ from powerline.bindings.ipython.since_7 import PowerlinePrompts
 c = get_config()
 c.TerminalInteractiveShell.simple_prompt = False
 c.TerminalInteractiveShell.prompts_class = PowerlinePrompts
-
+c.InteractiveShellApp.extensions = ['dotfiles']
