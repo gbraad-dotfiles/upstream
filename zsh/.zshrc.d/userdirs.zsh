f376c026bd4314f0a6d321f20c9c04648f48839f
Gerard Braad <me@gbraad.nl>
2025-05-29 19:38:32 +0800

    [skip actions] sudo chown userdirs when needed

--- zsh/.zshrc.d/userdirs.zsh
+++ zsh/.zshrc.d/userdirs.zsh
@@ -5,7 +5,30 @@ dotini() {
   git config -f $CONFIG "$@"
 }
 
+chown_if_needed() {
+  local expected_user="$1"
+  local target="$2"
+  local current_owner
+
+  if [ -z "$expected_user" ] || [ -z "$target" ]; then
+    return 1
+  fi
+
+  if [ ! -e "$target" ]; then
+    return 2
+  fi
+
+  current_owner=$(stat -c '%U' "$target")
+  if [ "$current_owner" != "$expected_user" ]; then
+    sudo chown -R "$expected_user" "$target"
+  fi
+}
+
 if [[ $(dotini --get "dotfiles.userdirs") == true ]]; then
+  chown_if_needed ${USER} ${HOME}/Projects
+  chown_if_needed ${USER} ${HOME}/Documents
+  chown_if_needed ${USER} ${HOME}/Downloads
+
   mkdir -p ${HOME}/Projects
 
   if which xdg-user-dirs-update >/dev/null 2>&1; then
