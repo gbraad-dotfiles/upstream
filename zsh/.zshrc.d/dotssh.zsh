c39b198eb258aefcf1e6ef30674bf6654a4bd240
Gerard Braad <me@gbraad.nl>
2025-07-07 19:50:34 +0800

    [skip actions] install with dotscreen

--- zsh/.zshrc.d/dotssh.zsh
+++ zsh/.zshrc.d/dotssh.zsh
@@ -12,11 +12,35 @@ dotssh() {
   ssh -X -t ${ADDRESS} "export LANG=en_US.UTF-8; export LC_ALL=en_US.UTF-8; export TERM=xterm-256color; dotfiles source; $@"
 }
 
-dotscreen() {
-  if [ $# -lt 1 ]; then
-    echo "Usage: $0 <address>"
-    return 1
-  fi 
+#dotscreen() {
+#  if [ $# -lt 1 ]; then
+#    echo "Usage: $0 <address>"
+#    return 1
+#  fi 
+#
+#  dotssh $1 screen
+#}
 
-  dotssh $1 screen
-}  
+dotscreen() {
+  local userhost="$1"
+  if [[ -z "$userhost" ]]; then
+    echo "Usage: dotscreen user@host"
+    return 1
+  fi
+
+  ssh -X -t "$userhost" '
+    export LANG=en_US.UTF-8
+    export LC_ALL=en_US.UTF-8
+    export TERM=xterm-256color
+
+    if command -v dotfiles >/dev/null 2>&1 && typeset -f dotfiles >/dev/null 2>&1; then
+      dotfiles source; screen
+    else
+      if [ ! -d "$HOME/.dotfiles" ]; then
+        git clone https://github.com/gbraad-dotfiles/upstream.git ~/.dotfiles --depth 2
+      fi
+      "$HOME/.dotfiles/activate.sh" screen
+    fi
+  '
+}
+  
