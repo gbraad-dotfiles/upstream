bb3e2b756760314a3b72e1669fb9c13187796c4d
Gerard Braad <me@gbraad.nl>
2025-07-09 12:16:54 +0800

    [skip actions] combine dotssh and dotscreen

--- zsh/.zshrc.d/dotssh.zsh
+++ zsh/.zshrc.d/dotssh.zsh
@@ -2,45 +2,36 @@
 
 dotssh() {
   if [ $# -lt 2 ]; then
-    echo "Usage: $0 <address> <command>"
+    echo "Usage: $0 <user@host> <command>"
     return 1
   fi 
 
   local ADDRESS=$1
   shift 1
+  local COMMAND='
+export LANG=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export TERM=xterm-256color
 
-  ssh -X -t ${ADDRESS} "export LANG=en_US.UTF-8; export LC_ALL=en_US.UTF-8; export TERM=xterm-256color; dotfiles source; $@"
+if command -v dotfiles >/dev/null 2>&1 && typeset -f dotfiles >/dev/null 2>&1; then
+ dotfiles source; __ARGS__
+else
+ if [ ! -d "${HOME}/.dotfiles" ]; then
+ git clone https://github.com/gbraad-dotfiles/upstream.git ~/.dotfiles --depth 2
+fi
+ "${HOME}/.dotfiles/activate.sh" __ARGS__
+fi
+'
+  local COMMAND_TO_SEND="${COMMAND//__ARGS__/$@}"
+  ssh -X -t -o StrictHostKeyChecking=no ${ADDRESS} ${COMMAND_TO_SEND}
 }
-
-#dotscreen() {
-#  if [ $# -lt 1 ]; then
-#    echo "Usage: $0 <address>"
-#    return 1
-#  fi 
-#
-#  dotssh $1 screen
-#}
 
 dotscreen() {
-  local userhost="$1"
-  if [[ -z "$userhost" ]]; then
-    echo "Usage: dotscreen user@host"
+  if [ $# -lt 1 ]; then
+    echo "Usage: $0 <user@host>"
     return 1
-  fi
+  fi 
 
-  ssh -X -t "$userhost" '
-    export LANG=en_US.UTF-8
-    export LC_ALL=en_US.UTF-8
-    export TERM=xterm-256color
-
-    if command -v dotfiles >/dev/null 2>&1 && typeset -f dotfiles >/dev/null 2>&1; then
-      dotfiles source; screen
-    else
-      if [ ! -d "$HOME/.dotfiles" ]; then
-        git clone https://github.com/gbraad-dotfiles/upstream.git ~/.dotfiles --depth 2
-      fi
-      "$HOME/.dotfiles/activate.sh" screen
-    fi
-  '
+  dotssh $1 screen
 }
-  
+
