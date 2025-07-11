ed936fea051863a5294b85d5c63d022bb62112a8
Gerard Braad <me@gbraad.nl>
2025-07-11 12:09:43 +0800

    [skip actions] always run tmux with config

--- zsh/.zshrc.d/tmux.zsh
+++ zsh/.zshrc.d/tmux.zsh
@@ -3,6 +3,7 @@ alias tmn='tmux new-session -s'
 alias tma='tmux attach-session -t'
 alias tmkill='tmux kill-session -t'
 
-if [ ! -L "${HOME}/.tmux.conf" ]; then
-  tmux() { command tmux -2 -f ${HOME}/.dotfiles/tmux/.tmux.conf $@ }
-fi
+#if [ ! -L "${HOME}/.tmux.conf" ]; then
+# Always run with forced config
+tmux() { command tmux -2 -f ${HOME}/.dotfiles/tmux/.tmux.conf $@ }
+#fi
