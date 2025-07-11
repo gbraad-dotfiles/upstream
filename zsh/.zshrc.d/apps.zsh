131218d58109056ac08bc116d751cbd17a361e33
Gerard Braad <me@gbraad.nl>
2025-07-11 15:52:53 +0800

    Apps able to run desktop entry with terminal

--- zsh/.zshrc.d/apps.zsh
+++ zsh/.zshrc.d/apps.zsh
@@ -348,12 +348,28 @@ if [[ $(dotini apps --get "apps.launcher") == true ]]; then
   bindkey "$shortcut" apps-launcher-command
 fi
 
+
 apps-export-desktop() {
   local appname="$1"
   local apptitle="$2"
   if [[ -z "$appname" || -z "$apptitle" ]]; then
     echo "Usage: apps-export-desktop <appname> <title>"
     return 1
+  fi
+
+  local desc_file="${_appsdefpath}/${appname}.md"
+
+  local has_rundesktop=0
+  if grep -Eq '^##\s+.*\brun-desktop\b' "$desc_file"; then
+    has_rundesktop=1
+  fi
+  local use_terminal exec_line
+  if [[ $has_rundesktop -eq 1 ]]; then
+    use_terminal="false"
+    exec_line="${HOME}/.dotfiles/bash/.local/bin/dot apps ${appname} desktop run"
+  else
+    use_terminal="true"
+    exec_line="${HOME}/.dotfiles/bash/.local/bin/dot apps ${appname} run"
   fi
 
   local desktop_dir="${HOME}/.local/share/applications"
@@ -365,10 +381,10 @@ apps-export-desktop() {
 Version=1.0
 Type=Application
 Name=${apptitle}
-Exec=${HOME}/.dotfiles/bash/.local/bin/dot apps ${appname} run
+Exec=${exec_line}
 Icon=prompt-icon-128.png
 Keywords=apps
-Terminal=false
+Terminal=${use_terminal}
 Categories=Utility;
 EOF
 
@@ -384,6 +400,13 @@ apps-export-service() {
   if [[ -z "$appname" || -z "$apptitle" ]]; then
     echo "Usage: apps-export-service <appname> <title>"
     return 1
+  fi
+
+  local desc_file="${_appsdefpath}/${appname}.md"
+
+  if ! grep -Eq '^##\s+.*\brun-service\b' "$desc_file"; then
+    echo "Cannot export service: No 'run-service' section found in $desc_file"
+    return 2
   fi
 
   local service_dir="${HOME}/.config/systemd/user"
