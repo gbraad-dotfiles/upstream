61389d63208d07c3afeded96d1012e6387a1081b
Gerard Braad <me@gbraad.nl>
2025-07-12 12:46:20 +0000

    [skip actions] add machinectl to start user session

--- .github/install.sh
+++ .github/install.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
 # clean workspace folder
-rm -rf /workspaces/upstream
-ln -s ~/.dotfiles /workspaces/upstream
+#rm -rf /workspaces/upstream
+#ln -s ~/.dotfiles /workspaces/upstream
+#
+#rm -rf /workspaces/dotfiles
+#ln -s ~/.dotfiles /workspaces/dotfiles
+#
+#rm -rf /workspaces/dotfiles-downstream
+#ln -s ~/.dotfiles /workspaces/dotfiles-downstream
 
-rm -rf /workspaces/dotfiles
-ln -s ~/.dotfiles /workspaces/dotfiles
-
-rm -rf /workspaces/dotfiles-downstream
-ln -s ~/.dotfiles /workspaces/dotfiles-downstream
-
-
-cd ~
-
-. /etc/os-release
-# we could do detection based on the lsb-release info to install more
+# Create user session
+sudo loginctl enable-linger gbraad
+sudo machinectl shell gbraad@ /usr/bin/echo "User session created"
 
 exit 0
