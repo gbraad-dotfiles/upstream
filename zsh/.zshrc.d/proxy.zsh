5032201510e9d3eb1d8045f14d430f3e570aa8c0
Gerard Braad <me@gbraad.nl>
2025-07-17 19:35:02 +0800

    [skip actions] supress output

--- zsh/.zshrc.d/proxy.zsh
+++ zsh/.zshrc.d/proxy.zsh
@@ -7,13 +7,16 @@ proxyselect() {
 proxy() {
 
   local PREFIX=$1
+  local output=$(dotini proxy --get "proxy.outpout")
 
   if [ -z "$PREFIX" ]; then
     PREFIX=$(proxyselect)
   fi
 
   if [ -z "$PREFIX" ] || [ "$PREFIX" = "-" ]; then
-    echo "Clearing proxy setting"
+    if [[ $output == true ]]; then
+      echo "Clearing proxy setting"
+    fi
     unset http_proxy
     unset https_proxy
     return
@@ -21,11 +24,13 @@ proxy() {
 
   local SERVER=$(dotini proxy --get servers.${PREFIX})
 
-  export http_proxy=${SERVER}
-  export https_proxy=${SERVER}
-
-  echo "Proxy settings"
-  set | grep -E "http_proxy|https_proxy"
+  export http_proxy="${SERVER}"
+  export https_proxy="${SERVER}"
+  
+  if [[ $output == true ]]; then
+    echo "Proxy settings"
+    set | grep -E "http_proxy|https_proxy"
+  fi
 }
 
 if [[ $(dotini proxy --get "proxy.aliases") == true ]]; then
