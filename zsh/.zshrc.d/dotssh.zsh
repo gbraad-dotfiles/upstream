9f68607002bef12e00ab732fd0e86bf014ac7759
Gerard Braad <me@gbraad.nl>
2025-07-18 12:05:36 +0800

    [skip actions] use stable for dot

--- zsh/.zshrc.d/dotssh.zsh
+++ zsh/.zshrc.d/dotssh.zsh
@@ -22,7 +22,7 @@ if command -v dotfiles >/dev/null 2>&1 && typeset -f dotfiles >/dev/null 2>&1; t
  dotfiles source; __ARGS__
 else
  if [ ! -d "${HOME}/.dotfiles" ]; then
-  git clone https://github.com/gbraad-dotfiles/upstream.git ~/.dotfiles --depth 2
+  git clone https://github.com/gbraad/dotfiles-stable.git ~/.dotfiles --depth 2
  fi
  "${HOME}/.dotfiles/activate.sh" __ARGS__
 fi
