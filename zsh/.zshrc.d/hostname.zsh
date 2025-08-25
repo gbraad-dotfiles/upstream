3d7c2cb1b13a36db2ffd1a5fecd576451267ed57
Gerard Braad <me@gbraad.nl>
2025-08-25 09:34:31 +0000

    Allow HOSTNAME override from user, eg code-tunnel needs

---
+++ zsh/.zshrc.d/hostname.zsh
@@ -0,0 +1,20 @@
+#!/bin/zsh
+HOSTNAME=$(cat ~/.hostname 2>/dev/null | tr -d '[:space:]')
+
+if [ -z "$HOSTNAME" ]; then
+  HOSTNAME=$(cat /etc/hostname 2>/dev/null | tr -d '[:space:]')
+fi
+
+if [ -z "$HOSTNAME" ]; then
+  HOSTNAME=$(hostname 2>/dev/null | tr -d '[:space:]')
+fi
+
+if [ -z "$HOSTNAME" ]; then
+  HOSTNAME=$(ip addr show eth0 2>/dev/null | grep 'inet ' | awk '{print $2}' | cut -d/ -f1)
+fi
+
+if [ -z "$HOSTNAME" ]; then
+  HOSTNAME="unknown"
+fi
+
+export HOSTNAME
