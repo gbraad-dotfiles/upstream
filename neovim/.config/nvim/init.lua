052464d67233d710e774591ff14557cffd29ca56
Gerard Braad <me@gbraad.nl>
2025-09-27 12:35:22 +0000

    [skip actions] refactor the nvim colors; faster processing

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -35,9 +35,86 @@ require("lazy").setup({
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     config = function()
+      -- Custom theme with our specific colors
+      local custom_theme = {
+        normal = {
+          a = { fg = '#007C00', bg = '#A2E600' },
+          b = { fg = '#c5c8c6', bg = '#373b41' },
+          c = { fg = '#b4b7b4', bg = '#303030' },
+          x = { fg = '#b4b7b4', bg = '#303030' },
+          y = { fg = '#c5c8c6', bg = '#585858' },
+          z = { fg = '#c5c8c6', bg = '#D0D0D0' }
+        },
+        insert = {
+          a = { fg = '#373b41', bg = '#FFFFFF' },
+          b = { fg = '#7CBCC0', bg = '#135C8C' },
+          c = { fg = '#7CBCC0', bg = '#135C8C' },
+          x = { fg = '#7CBCC0', bg = '#135C8C' },
+          y = { fg = '#111111', bg = '#1486B5' },
+          z = { fg = '#10537D', bg = '#8AD6FF' }
+        },
+        visual = {
+          a = { fg = '#c5c8c6', bg = '#606060' },
+          b = { fg = '#c5c8c6', bg = '#373b41' },
+          c = { fg = '#b4b7b4', bg = '#1d1f21' },
+          x = { fg = '#b4b7b4', bg = '#1d1f21' },
+          y = { fg = '#c5c8c6', bg = '#373b41' },
+          z = { fg = '#c5c8c6', bg = '#606060' }
+        },
+        replace = {
+          a = { fg = '#ffffff', bg = '#88E0FF' },
+          b = { fg = '#c5c8c6', bg = '#373b41' },
+          c = { fg = '#b4b7b4', bg = '#1d1f21' },
+          x = { fg = '#b4b7b4', bg = '#1d1f21' },
+          y = { fg = '#c5c8c6', bg = '#373b41' },
+          z = { fg = '#ffffff', bg = '#88E0FF' }
+        },
+        command = {
+          a = { fg = '#c5c8c6', bg = '#606060' },
+          b = { fg = '#c5c8c6', bg = '#373b41' },
+          c = { fg = '#b4b7b4', bg = '#1d1f21' },
+          x = { fg = '#b4b7b4', bg = '#1d1f21' },
+          y = { fg = '#c5c8c6', bg = '#373b41' },
+          z = { fg = '#c5c8c6', bg = '#606060' }
+        },
+        inactive = {
+          a = { fg = '#c5c8c6', bg = '#606060' },
+          b = { fg = '#c5c8c6', bg = '#373b41' },
+          c = { fg = '#b4b7b4', bg = '#1d1f21' },
+          x = { fg = '#b4b7b4', bg = '#1d1f21' },
+          y = { fg = '#c5c8c6', bg = '#373b41' },
+          z = { fg = '#c5c8c6', bg = '#606060' }
+        }
+      }
+      
+      -- Add tabline theme for normal and insert modes
+      custom_theme.normal.tabline = {
+        a = { fg = '#c5c8c6', bg = '#606060' },  -- buffers section
+        b = { fg = '#c5c8c6', bg = '#303030' },  -- empty sections
+        c = { fg = '#c5c8c6', bg = '#303030' },  -- middle section
+        x = { fg = '#c5c8c6', bg = '#303030' },
+        y = { fg = '#c5c8c6', bg = '#303030' },
+        z = { fg = '#c5c8c6', bg = '#606060' }   -- tabs section
+      }
+      
+      custom_theme.insert.tabline = {
+        a = { fg = '#ffffff', bg = '#88E0FF' },  -- buffers section in insert mode
+        b = { fg = '#c5c8c6', bg = '#303030' },
+        c = { fg = '#c5c8c6', bg = '#303030' },
+        x = { fg = '#c5c8c6', bg = '#303030' },
+        y = { fg = '#c5c8c6', bg = '#303030' },
+        z = { fg = '#ffffff', bg = '#88E0FF' }   -- tabs section in insert mode
+      }
+      
+      -- Copy tabline theme to other modes
+      custom_theme.visual.tabline = custom_theme.normal.tabline
+      custom_theme.replace.tabline = custom_theme.insert.tabline
+      custom_theme.command.tabline = custom_theme.normal.tabline
+      custom_theme.inactive.tabline = custom_theme.normal.tabline
+      
       require('lualine').setup({
         options = {
-          theme = 'powerline',
+          theme = custom_theme,
           component_separators = { left = '\u{e0b1}', right = '\u{e0b3}'},
           section_separators = { left = '\u{e0b0}', right = '\u{e0b2}'},
           disabled_filetypes = {},
@@ -61,12 +138,42 @@ require("lazy").setup({
           lualine_z = {}
         },
         tabline = {
-          lualine_a = {'buffers'},
+          lualine_a = {
+            {
+              'buffers',
+              buffers_color = {
+                active = function()
+                  local mode = vim.fn.mode()
+                  if mode == 'i' or mode == 'ic' or mode == 'ix' then
+                    return { fg = '#ffffff', bg = '#88E0FF' }
+                  else
+                    return { fg = '#c5c8c6', bg = '#606060' }
+                  end
+                end,
+                inactive = { fg = '#808080', bg = '#606060' }
+              }
+            }
+          },
           lualine_b = {},
           lualine_c = {},
           lualine_x = {},
           lualine_y = {},
-          lualine_z = {'tabs'}
+          lualine_z = {
+            {
+              'tabs',
+              tabs_color = {
+                active = function()
+                  local mode = vim.fn.mode()
+                  if mode == 'i' or mode == 'ic' or mode == 'ix' then
+                    return { fg = '#ffffff', bg = '#88E0FF' }
+                  else
+                    return { fg = '#c5c8c6', bg = '#606060' }
+                  end
+                end,
+                inactive = { fg = '#808080', bg = '#606060' }
+              }
+            }
+          }
         },
         extensions = {}
       })
@@ -77,47 +184,6 @@ require("lazy").setup({
 -- Basic settings
 vim.opt.laststatus = 2
 vim.opt.showmode = false
-
--- Fix tabline colors with autocmds
-vim.api.nvim_create_autocmd({"ModeChanged", "VimEnter"}, {
-  callback = function()
-    -- Delay to let lualine fully load first
-    vim.defer_fn(function()
-      local mode = vim.fn.mode()
-      if mode == 'i' then
-        vim.cmd([[
-          hi lualine_a_buffers_active ctermfg=15 ctermbg=39 guifg=#ffffff guibg=#88E0FF
-          hi lualine_z_tabs_active ctermfg=15 ctermbg=39 guifg=#ffffff guibg=#88E0FF
-        ]])
-        -- Set correct colors for insert mode (blue)
-        vim.cmd('hi! lualine_transitional_lualine_a_buffers_active_to_lualine_b_tabline ctermfg=39 guifg=#88E0FF')
-        vim.cmd('hi! lualine_transitional_lualine_z_tabs_active_to_lualine_c_tabline ctermfg=39 guifg=#88E0FF')
-        vim.cmd('hi! lualine_transitional_lualine_z_tabs_active_to_lualine_c_normal ctermfg=39 guifg=#88E0FF')
-        -- Set all buffer-related AND tab-related transitional highlights
-        for _, hl in ipairs(vim.fn.getcompletion('lualine_transitional', 'highlight')) do
-          if string.match(hl, 'buffer') or string.match(hl, 'tabs') then
-            vim.cmd('hi! ' .. hl .. ' ctermfg=39 guifg=#88E0FF')
-          end
-        end
-      else
-        vim.cmd([[
-          hi lualine_a_buffers_active ctermfg=15 ctermbg=241 guifg=#c5c8c6 guibg=#606060
-          hi lualine_z_tabs_active ctermfg=15 ctermbg=241 guifg=#c5c8c6 guibg=#606060
-        ]])
-        -- Set correct colors for normal mode (grey)
-        vim.cmd('hi! lualine_transitional_lualine_a_buffers_active_to_lualine_b_tabline ctermfg=241 guifg=#606060')
-        vim.cmd('hi! lualine_transitional_lualine_z_tabs_active_to_lualine_c_tabline ctermfg=241 guifg=#606060')
-        vim.cmd('hi! lualine_transitional_lualine_z_tabs_active_to_lualine_c_normal ctermfg=241 guifg=#606060')
-        -- Set all buffer-related transitional highlights  
-        for _, hl in ipairs(vim.fn.getcompletion('lualine_transitional', 'highlight')) do
-          if string.match(hl, 'buffer') then
-            vim.cmd('hi! ' .. hl .. ' ctermfg=241 guifg=#606060')
-          end
-        end
-      end
-    end, 100)
-  end
-})
 
 -- Appearance
 vim.cmd('colorscheme Tomorrow-Night-Bright')
@@ -135,15 +201,6 @@ vim.cmd([[
 hi! VertSplit guifg=bg guibg=bg gui=NONE
 hi! NonText guifg=bg
 hi Normal ctermbg=black
-
-" Fix tabline colors - background should be #606060, separators should be light grey
-hi TabLine ctermfg=250 ctermbg=241 guifg=#c5c8c6 guibg=#606060
-hi TabLineFill ctermfg=241 ctermbg=241 guifg=#606060 guibg=#606060
-hi TabLineSel ctermfg=15 ctermbg=241 guifg=#ffffff guibg=#606060
-
-" Override lualine tabline colors to prevent green bleeding
-hi lualine_a_buffers_active_buffers ctermfg=250 ctermbg=241 guifg=#ffffff guibg=#606060
-hi lualine_a_buffers_inactive_buffers ctermfg=8 ctermbg=241 guifg=#808080 guibg=#606060
 ]])
 
 -- NERDTree settings
